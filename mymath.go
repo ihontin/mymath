ea1f0c98235e71d028eeddb6005767db6bb5d4d7
ihontin <kononenkoas@mail.ru>
2023-08-27 10:36:13 +0300

    everything thenge

--- mymath.go
+++ mymath.go
@@ -7,18 +7,23 @@ var Stosem = 10.7
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
+
 func Ceil(x float64) float64 {
 	return math.Ceil(x)
 }
+
 func Floor(x float64) float64 {
 	return math.Floor(x)
 }
+
 func Pow(x, y float64) float64 {
 	return math.Pow(x, y)
 }
+
 func Max(x, y float64) float64 {
 	return math.Max(x, y)
 }
+
 func Min(x, y float64) float64 {
 	return math.Min(x, y)
 }
